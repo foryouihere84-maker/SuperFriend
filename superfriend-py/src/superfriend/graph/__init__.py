c5de2049d4b9e134ca41947c667b52efb3a1affe
iherefor <foryouihere84@gmail.com>
2026-06-06 00:26:40 +0800

    Delete superfriend-py directory

--- superfriend-py/src/superfriend/graph/__init__.py
+++
@@ -1,7 +0,0 @@
-from superfriend.graph.state import AgentState, PlannerState, ReflectionState
-from superfriend.graph.main_graph import build_agent_graph, create_default_agent
-
-__all__ = [
-    "AgentState", "PlannerState", "ReflectionState",
-    "build_agent_graph", "create_default_agent",
-]
--- superfriend-py/src/superfriend/strategy/__init__.py
+++
@@ -1,9 +0,0 @@
-from superfriend.strategy.base import ChatModeStrategy, OnResponseFn
-from superfriend.strategy.lite import LiteChatStrategy
-from superfriend.strategy.medium import MediumChatStrategy
-from superfriend.strategy.complex import ComplexChatStrategy
-
-__all__ = [
-    "ChatModeStrategy", "OnResponseFn",
-    "LiteChatStrategy", "MediumChatStrategy", "ComplexChatStrategy",
-]
